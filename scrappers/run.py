41543f5edb29bacddbbc60fc022af06dddefb241
sajid1189 <sajidur89@gmail.com>
2019-07-17 17:15:14 +0200

    multiprocessing used

--- scrappers/run.py
+++ scrappers/run.py
@@ -6,11 +6,11 @@ from scrappers.saturn_spider import *
 
 
 if __name__ == '__main__':
-    # saturn_proc = Process(target=SaturnSpider().start)
-    # saturn_proc.start()
+    saturn_proc = Process(target=SaturnSpider().start)
+    saturn_proc.start()
 
     mediamarkt_proc = Process(target=MediaMarktSpider().start)
     mediamarkt_proc.start()
 
-    # nb_proc = Process(target=NotebooksBilligerSpider().start)
-    # nb_proc.start()
+    nb_proc = Process(target=NotebooksBilligerSpider().start)
+    nb_proc.start()
